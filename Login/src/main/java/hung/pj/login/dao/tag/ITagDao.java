118314158e79e63bcf890d396f849c737cf17ac3
Em yeu anh Hung nhieu lam <muxica2411@gmail.com>
2023-09-12 00:04:58 +0700

    Create Dao Tags

--- Login/src/main/java/hung/pj/login/dao/tag/ITagDao.java
+++ Login/src/main/java/hung/pj/login/dao/tag/ITagDao.java
@@ -1,5 +1,17 @@
 package hung.pj.login.dao.tag;
 
-public interface ITagDao {
+import hung.pj.login.model.TagModel;
 
+import java.util.List;
+
+public interface ITagDao {
+    void addTag(List<TagModel> tagModels);
+
+    List<TagModel> getAllTag();
+    List<TagModel> getTagsByName(String tagName);
+
+
+    void updateTag(TagModel tagModel);
+
+    void deleteTag(int tagId);
 }
